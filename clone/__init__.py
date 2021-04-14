09ee9c03801ff86013653e241d2f1542c45b2896
arnu515 <arnu5152@gmail.com>
2021-04-14 11:30:49 +0530

    use os.walk instead of relying to tar

--- clone/__init__.py
+++ clone/__init__.py
@@ -13,8 +13,8 @@ class DataModel(pydantic.BaseModel):
     branch: typing.Optional[str] = "master"
     sid: typing.Optional[str] = None
 
-async def extract_github_repo_background_task(data, file_path: str, folder_path: str, post_url: typing.Optional[str], io_url: typing.Optional[str] = None):
-    async for i in extract_github_repo(file_path, folder_path):
+async def extract_github_repo_background_task(data, file_path: str, folder_path: str, branch: str, post_url: typing.Optional[str], io_url: typing.Optional[str] = None):
+    async for i in extract_github_repo(file_path, folder_path, branch):
         if i.get("main"):
             continue
         if post_url:
@@ -32,11 +32,11 @@ def download_gh_repo(repo: str = fastapi.Query(...), branch: typing.Optional[str
     return fastapi.responses.FileResponse(file_path)
 
 @app.post("/clone")
-def download_gh_repo(tasks: fastapi.BackgroundTasks, post_url: typing.Optional[str] = fastapi.Query(None), io_url: typing.Optional[str] = fastapi.Query(None), tar: typing.Optional[str] = fastapi.Query(None), data: DataModel = fastapi.Body(...), x_gh_token: typing.Optional[str] = fastapi.Header(None)):
+def download_gh_repo_post(tasks: fastapi.BackgroundTasks, post_url: typing.Optional[str] = fastapi.Query(None), io_url: typing.Optional[str] = fastapi.Query(None), tar: typing.Optional[str] = fastapi.Query(None), data: DataModel = fastapi.Body(...), x_gh_token: typing.Optional[str] = fastapi.Header(None)):
     file_path, folder_path = dl_github_repo(data.repo, x_gh_token or None, data.branch)
     if file_path is None:
         raise fastapi.exceptions.HTTPException(404, "Repo not found")
-    tasks.add_task(extract_github_repo_background_task, data, file_path, folder_path, post_url, io_url)
+    tasks.add_task(extract_github_repo_background_task, data, file_path, folder_path, data.branch, post_url, io_url)
 
     return fastapi.responses.FileResponse(file_path) if tar is not None else data.repo
 
@@ -65,10 +65,10 @@ def dl_github_repo(repo: str, token: typing.Optional[str] = None, branch="master
         f.write(res.content)
     return path, os.path.join(basepath, "repos", folder)
 
-async def extract_github_repo(path: str, folder_path: str):
-    contents = subprocess.Popen("cd " + folder_path + " && tar -xvzf " + path, shell=True, stdout=subprocess.PIPE).communicate()[0].decode().split("\n")[0:-1]
-    folder = contents.pop(0)[0:-1] # gets the first item, removes it from the list, and removes the trailing slash.
-    yield {"name":folder, "is_folder": True, "main": True}
-    for i in contents:
-        is_folder = i.endswith("/")
-        yield {"name": i, "is_folder": is_folder, "path": os.path.join(folder_path, i)}
+async def extract_github_repo(path: str, folder_path: str, branch: str):
+    subprocess.call("cd " + folder_path + " && tar -xvzf " + path, shell=True)
+    for parent, _, files in os.walk(path[0:path.rfind(".tar.gz")] + f"-{branch}"):
+        folder = parent.replace(folder_path, "", 1)
+        yield {"name": folder, "is_folder": True, "path": parent}
+        for file in files:
+            yield {"name": os.path.join(folder, file), "is_folder": False, "path": os.path.join(folder_path, folder.replace("/", "", 1), file)}
